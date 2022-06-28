89a7e72b4201f41931099b972051fb515411e221
alispnor@gmail.com <Aliyemen>
2022-06-28 16:46:46 -0300

    melhoria no mask de telefone

--- src/components/sheared/FieldsetMk.tsx
+++ src/components/sheared/FieldsetMk.tsx
@@ -1,5 +1,6 @@
 import { styled } from '@stitches/react';
 import { gray } from '@radix-ui/colors';
+import InputMask from "react-input-mask";
 
 type InputProps = {
     label: string;
@@ -35,15 +36,37 @@ const Fieldset = styled('fieldset', {
     color: gray.gray10,
     boxShadow: `0 0 0 1px ${gray.gray8}`,
     height: 35,
+    width:`${(props: { width: any; }) => props.width}px`,
+    '&:focus': { boxShadow: `0 0 0 2px ${gray.gray9}` },
+  });
+  const InputMaskMk = styled(InputMask, {
+    all: 'unset',
+    flex: '1 0 auto',
+    borderRadius: 6,
+    padding: '0 10px',
+    fontSize: 15,
+    lineHeight: 1,
+    color: gray.gray10,
+    boxShadow: `0 0 0 1px ${gray.gray8}`,
+    height: 35,
+    width:`${(props: { width: any; }) => props.width}px`,
     '&:focus': { boxShadow: `0 0 0 2px ${gray.gray9}` },
   });
 export function FieldsetMk(props: InputProps) {
+    function getInput(props :InputProps){
+      if( props.model == 'mobile'){
+                
+        return <InputMaskMk width={props.width} mask='(99) 99999-9999'  id={props.model} defaultValue="" />
+      }
+      return  <Input width={props.width} id={props.model} defaultValue="" />
+    }
     return (
         <Fieldset>
           <Label htmlFor={props.model}>{props.label}</Label>
-          <Input style={{
-            width: `${props.width}px`
-          }} id={props.model} defaultValue="" />
+          {
+           getInput(props)
+          }
+          
         </Fieldset>
     )
 }
